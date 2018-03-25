9155dceab94eeef1c20f1cfd339c0680af882ff5
MikaGuraN <1069952275@qq.com>
2018-03-25 12:06:03 +0800

    更新更新更

---
+++ 应用/src/main/java/hl4a/app/界面/用户信息界面.java
@@ -0,0 +1,13 @@
+package hl4a.app.界面;
+
+import 间.安卓.组件.界面;
+import android.os.Bundle;
+
+public class 用户信息界面 extends 界面 {
+
+    @Override
+    public void 界面创建事件(Bundle $恢复) {
+        
+    }
+    
+}
